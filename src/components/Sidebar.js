d964390858d73ca5b8ee483900691aca98be40a3
Ali Kerem Ata <96057812+sabakukhyraa@users.noreply.github.com>
2022-12-08 17:14:21 +0300

    Sidebar created

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,19 +1,61 @@
+import Icons from "./Icons";
+
 export default function Sidebar() {
   return (
-    <div className="fixed top-0 left-0 h-2/3 bg-back-front font-semibold text-faint">
-      <div className="relative inline-block">
-        <img src="/assets/logo.png" alt="" />
-        <ul>
+    <div className="fixed h-[750px] top-0 left-0 flex flex-col justify-between items-center bg-back-front font-semibold ">
+      <div>
+        <img className="md:m-8 md:my-12 logo" src="/assets/logo.png" alt="logo" />
+        <ul className="text-faint flex flex-col navbar gap-y-4 text-2xl">
+          <li className="active">
+            <span></span>
+            <div>
+              <Icons iconName="Dashboard" />
+              <a className="md:inline-block hidden" href="/">
+                Dashboard
+              </a>
+            </div>
+          </li>
           <li>
             <span></span>
-            Dashboard
+            <div>
+              <Icons iconName="Wallet" />
+              <a className="md:inline-block hidden" href="/">
+                Wallet
+              </a>
+            </div>
           </li>
-          <li>Wallet</li>
-          <li>Cards</li>
-          <li>Settings</li>
-          <li>FAQ</li>
+          <li>
+            <span></span>
+            <div>
+              <Icons iconName="Cards" />
+              <a className="md:inline-block hidden" href="/">
+                Cards
+              </a>
+            </div>
+          </li>
+          <li>
+            <span></span>
+            <div>
+              <Icons iconName="Settings" />
+              <a className="md:inline-block hidden" href="/">
+                Settings
+              </a>
+            </div>
+          </li>
+          <li>
+            <span></span>
+            <div>
+              <Icons iconName="FAQ" />
+              <a className="md:inline-block hidden" href="/">
+                FAQ
+              </a>
+            </div>
+          </li>
         </ul>
       </div>
+      <span className="text-extraFaint text-xl font-semibold mb-2 md:inline hidden">
+        Lorem ipsum dolor
+      </span>
     </div>
   );
 }
