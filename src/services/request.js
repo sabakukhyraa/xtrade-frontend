421c407e7398133e7c72809ad1b247c4ad3581e7
Ali Kerem Ata <96057812+sabakukhyraa@users.noreply.github.com>
2022-12-09 17:20:32 +0300

    api requests prepared

---
+++ src/services/request.js
@@ -0,0 +1,25 @@
+function Request(url, data = false, method = "GET") {
+  return new Promise(async (resolve, reject) => {
+
+    const options = {
+      method,
+    };
+
+    if (data && method === "POST") {
+      options.body = JSON.stringify(data);
+    }
+
+    const response = await fetch(url, options);
+    const result = await response.json();
+
+    if (response.ok && response.status === 200) {
+      resolve(result);
+    } else {
+      reject(result);
+    }
+
+  });
+}
+
+export const post = (url, data) => Request(url, data, "POST");
+export const get = (url) => Request(url);
